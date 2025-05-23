6e02a918cadef3f0a714f15721b957e270a3dd27
kyiv14 <kyiv14@gmail.com>
2025-05-23 14:14:25 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
-
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-import re
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = "7490249052:AAEaldElMOFFJwn9WIvuSR0bx6tFaebeR0k"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         "👋 Привет! Используй команды:
 "
-        "/gmail — генерация точек
+        "/email - генерация Gmail с точками
 "
-        "/gdrive — прямая ссылка
+        "/link - конвертировать ссылку Google Drive
 "
-        "/qr — декодировать QR"
+        "/qr - распознать QR-код с картинки"
     )
 
 def main():
